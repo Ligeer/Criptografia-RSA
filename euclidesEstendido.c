8afaf85617d6cf72f4b576ab22674c393fd9101e
alechaito <alechaito@gmail.com>
2016-05-02 00:25:43 -0300

    Update euclidesEstendido.c

--- euclidesEstendido.c
+++ euclidesEstendido.c
@@ -1,12 +1,17 @@
-int euclidesEstendido(int a, int b){
-    /*encontrando d = a*X + b*Y */
-    int dividendo, divisor;
-    dividendo = a;
-    divisor = b;
-    if(divisor == 0){
-        return dividendo;
+/*Calculando euclides conforme a aula, d = a*X + b*Y | d = mdc(a,b)/
+int* ext(int a, int b){
+    int *vetEuclides;
+    int d = 0;
+    vetEuclides = (int*) malloc(3*sizeof(int));
+    if (b == 0){
+        vetEuclides[0] = 1;
+        vetEuclides[1] = 0;
+        vetEuclides[2] = a;
+        return vetEuclides;
     }
-    else{
-        return mdc(b, a%b);
-    }
+    vetEuclides = ext(b, a%b);
+    vetEuclides[0] = b;
+    vetEuclides[1] = a - (floor(a/b)*b);
+    vetEuclides[2] = vetEuclides[2];
+    return vetEuclides;
 }
