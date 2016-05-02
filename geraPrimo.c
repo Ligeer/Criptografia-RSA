0a3fa55b3b5e4fc15e3f3af10855bdf823a070f8
alechaito <alechaito@gmail.com>
2016-05-01 22:15:11 -0300

    Update geraPrimo.c

--- geraPrimo.c
+++ geraPrimo.c
@@ -12,7 +12,7 @@ int geraPrimo();
 
 int main(){
     int r = 0;
-    /*Ele ta errando de vez em quando não sei porque...*/
+    /*Ele ta errando de vez em quando não sei porque mas acerta muito mais que erra...*/
     r = geraPrimo();
     printf("Primo encontrado: %d \n", r);
     return 0;
@@ -22,10 +22,10 @@ int primalidade(int n){
     int i = 0;
     int a = 0;
     int result;
-    while(i<=100){
-        a = geraNumeroMax(n);
-        result = expModular(a, b, n);
-        if (result != 1 % n){
+    while(i<=100){ /*Rodando 100 vezes para diminuir em 1/2*100 a chance de erro*/
+        a = geraNumeroMax(n-1); /*Gerando numero entre 1 e n-1*/
+        result = expModular(a, b, n); 
+        if (result != 1 % n){ /*Verificando inverso multiplicativo*/
             return 0;
         }
         i++;
@@ -36,15 +36,14 @@ int primalidade(int n){
 int geraPrimo(){
     int primo;
     primo = geraNumeroRandom();
-    while(primalidade(primo) != 1){
-        primo = geraNumeroMax(50);
+    while(primalidade(primo) != 1){/*Em quanto primalidade não for igual a 1 que é verdadeiro*/
+        primo = geraNumeroMax(50); /*Gerando numero aleatorio entre 1 e X*/
         printf("procurando... \n");
     }
     return primo;
 }
 
 int geraNumeroMax(int n){
-    n = n-1;
     srand((unsigned)time(NULL));
     return rand() % n + 1;
 };
@@ -54,6 +53,7 @@ int geraNumeroRandom(){
     return rand();
 };
 
+/*Calculado conforme a aula*/
 int expModular(int a, int b, int n){
     int z = 0;
     if(b==0){
