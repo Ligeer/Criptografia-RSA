33a2a057e974d97e67004b494073bf4101df4053
Luan <overblessbms@gmail.com>
2016-05-16 23:13:57 -0300

    Adicionada funcao subtrai

--- rsaFinal.cpp
+++ rsaFinal.cpp
@@ -16,24 +16,89 @@ typedef struct numPrimo Numprimo;
 int geraNumeroMax(int n);
 Numprimo *geraPrimoGrande();
 Numprimo * criaLista(int valor);
+Numprimo * subtrai(Numprimo * a, Numprimo * b);
 int primalidade(int n);
 void geraSeed();
 void geraLista(Numprimo *head);
 void printLista(Numprimo *head);
+void insereLista(Numprimo *head, int num);
+bool isNegativo(Numprimo * head);
 
 //OPERAÇÕES
 //*FALTA COMEÇAR IMPLEMENTAR AS OPERAÇÕES
 
 int main() {
-    Numprimo * head;
+    Numprimo * head, *head2;
 
-    head = criaLista(0); // 0 indica o sinal do numero
+    head = criaLista(1); // 0 indica o sinal do numero
+    head2 = criaLista(1);
 
     geraSeed();
     geraLista(head);
-    printLista(head);
+    geraLista(head2);
 
+    printLista(head);
+    printf("\n");
+    printLista(head2);
+    printf("\n");
+    printLista(subtrai(head, head2));
     return 0;
+}
+
+bool isNegativo(Numprimo * head) {
+    return head->num >= 0 ? true : false;
+}
+
+Numprimo * soma(Numprimo * a, Numprimo * b) {
+    /*colocar funcao  */
+    return NULL;
+}
+
+/* Recebe dois tipos Numprimo e subtrai a - b e retorna */
+Numprimo * subtrai(Numprimo * a, Numprimo * b) {
+    Numprimo * aux1, * aux2, * c;
+    int subtracao;
+
+/* Tratamentos a serem feitos:
+    a - 0 = a
+    0 - b = -b
+
+    retornar numeros negativos
+    sequencias grandes de 0
+ */
+        aux1 = a->ante; // elemento antes da cabeca = final
+        aux2 = b->ante;
+
+
+
+        c = criaLista(1);
+        while(aux1 != a && aux2 != b) {
+            subtracao = 0;
+            if(aux1->num - aux2->num < 0) {
+                // tenta pedir emprestado um do lado esquerdo
+                if(aux1->ante != a) {
+                    subtracao = 10; // pega emprestado o algarismo esquerdo
+                    aux1->ante->num -= 1; // paga com -1 no algarismo esquerdo
+                    subtracao += aux1->num; // valor com o emprestimo
+                }
+                if(aux2->ante != b) {
+                    subtracao -= aux2->num; // subtrai
+                }
+            } else { // faz a subtracao normal
+                subtracao = aux1->num - aux2->num;
+            }
+
+            insereLista(c, subtracao);
+
+            if(aux1 != a) {
+                aux1 = aux1->ante;
+            }
+            if(aux2 != b) {
+                aux2 = aux2->ante;
+            }
+        }
+
+    return c;
 }
 
 int geraNumeroMax(int n){
@@ -52,7 +117,7 @@ Numprimo * criaLista(int valor) {
     return head;
 }
 
-void insereLista (Numprimo *head, int num){
+void insereLista(Numprimo *head, int num){
     Numprimo *novo;
     novo = (Numprimo*) malloc(sizeof(Numprimo));
     novo->num = num; //acessa nova e bota o endereço da pessoa1 lá dentro;
@@ -73,7 +138,7 @@ void insereLista (Numprimo *head, int num){
 void geraLista(Numprimo *head) {
     int i;
     i=0;
-    while (i != 10) { // deve ser colocado 256 digitos
+    while (i != 20) { // deve ser colocado 256 digitos
         insereLista(head, geraNumeroMax(9)); //GERANDO NUMEROS DE 1-9 E GUARDANDO NA LISTA
         i++;
     }
@@ -86,9 +151,8 @@ void printLista(Numprimo *head){
     ini = head->prox;
     i=0;
     while(ini != head){
-        printf("Indice: %d |Num: %d \n", i, ini->num);
+        printf("%i ", ini->num);
         ini = ini->prox;
-        printf("____________\n");
         i++;
     }
 }
