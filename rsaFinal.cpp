7b8d05629dafd241ea106a4ba3cddf5529c96e95
Luan <overblessbms@gmail.com>
2016-05-18 16:24:16 -0300

    Consertado bug subtracao
    Consertado bug e removido parte corrompida do arquivo

--- rsaFinal.cpp
+++ rsaFinal.cpp
@@ -14,38 +14,43 @@ typedef struct numPrimo Numprimo;
 
 //FUNÇÕES
 int geraNumeroMax(int n);
+int numDigitos(Numprimo * a);
 Numprimo *geraPrimoGrande();
 Numprimo * criaLista(int valor);
-Numprimo * subtrai(Numprimo * a, Numprimo * b);
 int primalidade(int n);
 void geraSeed();
 void geraLista(Numprimo *head, int numeros);
 void printLista(Numprimo *head);
 void insereLista(Numprimo *head, int num);
-Numprimo *soma(Numprimo *head1, Numprimo *head2);
+void pedeEmprestado(Numprimo * a);
+Numprimo * soma(Numprimo *head1, Numprimo *head2);
+Numprimo * maior(Numprimo * a, Numprimo * b);
+Numprimo * subtrai(Numprimo * a, Numprimo * b);
+
 
 //OPERAÇÕES
 //*FALTA COMEÇAR IMPLEMENTAR AS OPERAÇÕES
 
 int main() {
-    Numprimo * head1, *head2;
+    Numprimo * head1, * head2;
 
-    head1 = criaLista(0); // 0 indica o sinal do numero
-    head2 = criaLista(0);
+    head1 = criaLista(1); // 0 indica o sinal do numero
+    head2 = criaLista(1);
 
     geraSeed();
-    geraLista(head1, 5);
-    geraLista(head2, 5);
+    geraLista(head1, 25);
+    geraLista(head2, 30);
+
 
     printLista(head1);
     printf("\n");
     printLista(head2);
     printf("\n");
 
-    printLista(subtrai(head1, head2));
+    printLista(soma(head1, head2));
+
     return 0;
 }
-
 
 Numprimo *soma(Numprimo *head1, Numprimo *head2) {
     Numprimo *headaux1 = head1->ante;
@@ -86,51 +91,117 @@ Numprimo *soma(Numprimo *head1, Numprimo *head2) {
     return soma;
 }
 
-/* Recebe dois tipos Numprimo e subtrai a - b e retorna */
+/* Recebe dois Numprimos e retorna o endereço do maior */
+Numprimo * maior(Numprimo * a, Numprimo * b) {
+    int x;
+    Numprimo * aux1, * aux2;
+
+    //tratamento numeros com sinais opostos
+    if((a->num + b->num) == 0) { // se os dois tem sinais diferentes
+        return a->num == 1 ? a : b;
+    }
+
+    // é maior o que tem mais digitos já que nao armazenamos zeros
+    if(numDigitos(a) > numDigitos(b)) {
+        return a;
+    }
+    if(numDigitos(a) < numDigitos(b)) {
+        return b;
+    }
+
+    x = 0;
+    aux1 = a->prox;
+    aux2 = b->prox;
+
+    while(x == 0) { // só entra no looping se possui mesma quantidade de digitos
+        if(aux1->num > aux2->num) {
+            return a;
+        }
+        if (aux1->num < aux2->num) {
+            return b;
+        }
+        aux1 = aux1->prox;
+        aux2 = aux2->prox;
+    }
+}
+
+/* Recebe um ponteiro para um Numprimo e conta a quantidades de
+dígitos doo Numero
+ */
+int numDigitos(Numprimo * a) {
+    Numprimo * aux;
+    int i;
+
+    i = 0;
+    aux = a->prox;
+    while(aux != a) {
+        aux = aux->prox;
+        i++;
+    }
+    return i;
+}
+
 Numprimo * subtrai(Numprimo * a, Numprimo * b) {
-    Numprimo * aux1, * aux2, * c;
+    Numprimo * aux1, * aux2, * empresta, * c;
     int subtracao;
 
-/* Tratamentos a serem feitos:
-    a - 0 = a
-    0 - b = -b
+    // se b > a entao o algoritmo nao funciona, entao mudamos
+    // o sinal do número e chamamos a funcao com os argumentos invertidos
+    if(maior(a, b) == b) {
+        c = subtrai(b, a);
+        c->num *= -1; // negativa o resultado, já que b > a
+        return c;
+    }
 
-    retornar numeros negativos
-    sequencias grandes de 0
- */
-        aux1 = a->ante; // elemento antes da cabeca = final
-        aux2 = b->ante;
+    aux1 = a->ante; // elemento antes da cabeca = final
+    aux2 = b->ante;
+    c = criaLista(1);
 
+    while(aux1 != a || aux2 != b) {
+        subtracao = 0;
 
+        if(aux1->num - aux2->num < 0) {
+            pedeEmprestado(aux1); // pedimos emprestado para os dígitos anteriores
+            subtracao = 10 + aux1->num - aux2->num ; // subtração feita com o empréstimo
+        } else {
+            subtracao = aux1->num - aux2->num; // não precisa fazer o empréstimo, subtração normal
+        }
+        insereLista(c, subtracao); // insere na lista o valor subtraido
 
-        c = criaLista(1);
-        while(aux1 != a && aux2 != b) {
-            subtracao = 0;
-            if(aux1->num - aux2->num < 0) {
-                // tenta pedir emprestado um do lado esquerdo
-                if(aux1->ante != a) {
-                    subtracao = 10; // pega emprestado o algarismo esquerdo
-                    aux1->ante->num -= 1; // paga com -1 no algarismo esquerdo
-                    subtracao += aux1->num; // valor com o emprestimo
-                }
-                if(aux2->ante != b) {
-                    subtracao -= aux2->num; // subtrai
-                }
-            } else { // faz a subtracao normal
-                subtracao = aux1->num - aux2->num;
-            }
-
-            insereLista(c, subtracao);
-
-            if(aux1 != a) {
-                aux1 = aux1->ante;
-            }
-            if(aux2 != b) {
-                aux2 = aux2->ante;
-            }
+        // os dois ponteiros percorrerão para a esquerda até encontrar a cabeça da lista
+        if(aux1 != a) {
+            aux1 = aux1->ante;
+        }
+        if(aux2 != b) {
+            aux2 = aux2->ante;
         }
 
+    }
+
+    //agora uma das duas listas chegou na cabeça, a operação é só o próprio número
+    while(aux1 != a) {
+        insereLista(c, aux1->num);
+        aux1 = aux1->ante;
+    }
+
+    while(aux2 != b) {
+        insereLista(c, aux2->num);
+        aux2 = aux2->ante;
+    }
     return c;
+}
+
+/* Função utilizada na subtracao de um Numprimo
+pede emprestado um numero de uma casa, balanceando o valor
+na casa anterior
+ */
+void pedeEmprestado(Numprimo * a) {
+    if((a->ante->num) == 0) {
+        a->ante->num = 9;
+        pedeEmprestado(a->ante);
+    } else {
+        a->ante->num -= 1;
+    }
 }
 
 int geraNumeroMax(int n){
@@ -180,8 +251,14 @@ void printLista(Numprimo *head){
     int i;
     Numprimo *ini;
 
+
     ini = head->prox;
     i=0;
+
+    if(head->num == -1) {
+        printf("- ");
+    }
+
     while(ini != head){
         printf("%i ", ini->num);
         ini = ini->prox;
